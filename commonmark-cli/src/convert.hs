d567a8f1d7d7a989dc760e38321d209ef69335cc
John MacFarlane <jgm@berkeley.edu>
2018-03-22 20:50:22 -0700

    Revert "Nonworking first attempt at ANSI highlighting."
    This reverts commit 797f24a6f1580d031de692a04b6bb5492a1a012e.

--- commonmark-cli/src/convert.hs
+++ commonmark-cli/src/convert.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP                 #-}
-{-# LANGUAGE TupleSections       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
@@ -10,18 +9,10 @@ import           Commonmark.Extensions.Smart
 import           Commonmark.Extensions.Strikethrough
 import           Commonmark.Extensions.PipeTable
 import           Control.Monad
-import           Control.Monad.State
 import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Foldable              as F
-import qualified Data.Sequence              as Seq
 import qualified Data.Text.IO               as TIO
 import qualified Data.Map                   as M
 import qualified Data.Text                  as T
-import           System.Console.ANSI.Codes (setSGRCode,
-                                            SGR(..), ColorIntensity(..),
-                                            Color(..), ConsoleLayer(..),
-                                            Underlining(..),
-                                            ConsoleIntensity(..))
 import           Data.Text                  (Text)
 import           Lucid
 import           System.Environment
@@ -90,8 +81,9 @@ main = do
       case runWithSourceMap <$> res of
            Left e -> errExit e
            Right ((_ :: Html ()), sm) -> do
-             TIO.putStr $ highlightWith sm toks
-             TIO.putStr "\n"
+             BL.putStr $ renderBS styles
+             BL.putStr $ renderBS $ highlightWith sm toks
+             BL.putStr "\n"
   else
     if SourcePos `elem` opts then do
        res <- parser toks
@@ -110,54 +102,8 @@ errExit err = do
   hPrint stderr err
   exitWith (ExitFailure 1)
 
-highlightWith :: SourceMap -> [Tok] -> Text
-highlightWith sm ts =
-  mconcat $ evalState (mapM (renderTokANSI sm) ts)
-    TokFormat{
-        tokItalic = False
-      , tokBold = False
-      , tokStr = False
-      , tokCode = False
-      }
-
-data TokFormat = TokFormat
-     { tokItalic :: Bool
-     , tokBold   :: Bool
-     , tokStr    :: Bool
-     , tokCode   :: Bool
-     }
-
-tokFormatToSGR :: TokFormat -> [SGR]
-tokFormatToSGR fmt =
-  (SetItalicized (tokItalic fmt && tokStr fmt):) .
-  (SetConsoleIntensity (if tokStr fmt
-                           then if tokBold fmt
-                                    then BoldIntensity
-                                    else NormalIntensity
-                           else FaintIntensity):)
-  $ []
-
-renderTokANSI :: SourceMap -> Tok -> State TokFormat Text
-renderTokANSI (SourceMap sm) (Tok _ pos t) =
-  case M.lookup pos sm of
-       Nothing -> return t
-       Just (starts, ends) -> do
-         mapM_ updateSGRs $ map (True,) (F.toList starts) ++
-                            map (False,) (F.toList ends)
-         fmt <- get
-         return $ T.pack (setSGRCode (tokFormatToSGR fmt)) <> t
-
-  where updateSGRs :: (Bool, Text) -> State TokFormat ()
-        updateSGRs x = case x of
-                            (begin, "str")   -> modify $ \fmt ->
-                                fmt{ tokStr = begin }
-                            (_, "emph")   -> toggleItalicized
-                            (_, "strong") -> toggleBold
-                            _             -> return ()
-        toggleItalicized = do
-          modify $ \fmt -> fmt{ tokItalic = not (tokItalic fmt) }
-        toggleBold = do
-          modify $ \fmt -> fmt{ tokBold = not (tokBold fmt) }
+highlightWith :: SourceMap -> [Tok] -> Html ()
+highlightWith sm = pre_ . mconcat  . map (renderTok sm)
 
 renderTok :: SourceMap -> Tok -> Html ()
 renderTok (SourceMap sm) (Tok _ pos t) =

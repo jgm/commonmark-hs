68a9adc69e0f33e8a46ec122ee0e227a67f5dc5f
John MacFarlane <jgm@berkeley.edu>
2018-05-18 22:49:50 -0700

    More scaffolding for footnote extensions.

--- commonmark/src/Commonmark/Extensions/Footnote.hs
+++ commonmark/src/Commonmark/Extensions/Footnote.hs
@@ -6,14 +6,43 @@ module Commonmark.Extensions.Footnote
 where
 import Commonmark.Types
 import Commonmark.Syntax
--- import Commonmark.Inlines
--- import Data.Semigroup (Semigroup(..))
+import Commonmark.Blocks
+import Commonmark.Inlines
+import Commonmark.SourceMap
+import Control.Monad (mzero)
+import Data.Semigroup (Semigroup(..))
+import Data.Text.Lazy.Builder (Builder)
 
-footnoteSpec :: (Monad m, IsBlock il bl, IsInline il)
+footnoteSpec :: (Monad m, IsBlock il bl, IsInline il, HasFootnoteRef il)
              => SyntaxSpec m il bl
 footnoteSpec = SyntaxSpec
-  { syntaxBlockSpecs = []
+  { syntaxBlockSpecs = [footnoteBlockSpec]
   , syntaxBracketedSpecs = []
   , syntaxFormattingSpecs = []
-  , syntaxInlineParsers = []
+  , syntaxInlineParsers = [pFootnoteRef]
   }
+
+footnoteBlockSpec :: BlockSpec m il bl
+footnoteBlockSpec = BlockSpec
+     { blockType           = "Footnote"
+     , blockStart          = mzero
+     , blockCanContain     = const True
+     , blockContainsLines  = False
+     , blockParagraph      = False
+     , blockContinue       = \node -> mzero
+     , blockConstructor    = \node -> mzero
+     , blockFinalize       = \node node2 -> mzero
+     }
+
+pFootnoteRef :: (Monad m, HasFootnoteRef a) => InlineParser m a
+pFootnoteRef = fail "undefined"
+
+class HasFootnoteRef a where
+  footnoteRef :: a -> a
+
+instance HasFootnoteRef Builder where
+  footnoteRef x = "<sup>" <> x <> "</sup>"
+
+instance (HasFootnoteRef i, Monoid i)
+        => HasFootnoteRef (WithSourceMap i) where
+  footnoteRef x = (footnoteRef <$> x) <* addName "footnoteRef"

0b0a927adb7ab9393c8eb302162c5babde296f1f
John MacFarlane <jgm@berkeley.edu>
2018-06-11 11:37:24 -0700

    Removed meta from block list, in accord with spec change.
    see commonmark/CommonMark#527.

--- commonmark/src/Commonmark/Blocks.hs
+++ commonmark/src/Commonmark/Blocks.hs
@@ -893,7 +893,7 @@ startCond 6 = void $ try $ do
     "dt", "fieldset", "figcaption", "figure", "footer", "form", "frame",
     "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header",
     "hr", "html", "iframe", "legend", "li", "link", "main", "menu",
-    "menuitem", "meta", "nav", "noframes", "ol", "optgroup", "option",
+    "menuitem", "nav", "noframes", "ol", "optgroup", "option",
     "p", "param", "section", "source", "summary", "table", "tbody",
     "td", "tfoot", "th", "thead", "title", "tr", "track", "ul"])
   spaceTok

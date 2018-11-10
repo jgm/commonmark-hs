f087f1f0c13279f3d9ae1487d7ff1987638721cc
John MacFarlane <jgm@berkeley.edu>
2018-11-10 14:40:46 -0800

    Lucid: support footnotes.
    Also use 'coerce' to simplify code and remove duplication.

--- commonmark-lucid/src/Commonmark/Lucid.hs
+++ commonmark-lucid/src/Commonmark/Lucid.hs
@@ -24,6 +24,7 @@ import qualified Data.Text.Read as TR
 import Text.HTML.TagSoup
 import Control.Monad (unless, zipWithM_)
 import Data.Char (isSpace)
+import Data.Coerce (coerce)
 import Network.URI (isAllowedInURI, escapeURIString)
 import Commonmark.Types
 import Commonmark.Extensions.Math
@@ -122,58 +123,32 @@ newtype RangedHtml5 = RangedHtml5 {unRangedHtml5 :: Html ()}
   deriving (Show, Semigroup, Monoid)
 
 instance IsInline RangedHtml5 where
-  lineBreak = RangedHtml5 $ br_ [] <> "\n"
-  softBreak = RangedHtml5 "\n"
   str t = RangedHtml5 $ span_ $ toHtml t
+  escapedChar c = RangedHtml5 $ span_ $ toHtml (T.singleton c)
   entity t
     | illegalCodePoint t = RangedHtml5 $ span_ $ toHtml ("\xFFFD" :: Text)
     | otherwise = RangedHtml5 $ span_ $ toHtmlRaw t
-  escapedChar c = RangedHtml5 $ span_ $ toHtml (T.singleton c)
-  emph ils = RangedHtml5 $ em_ $ unRangedHtml5 ils
-  strong ils = RangedHtml5 $ strong_ $ unRangedHtml5 ils
-  link target title ils = RangedHtml5 $
-    a_ (href_ (escapeURI target) : [title_ title | not (T.null title)])
-    $ unRangedHtml5 ils
-  image target title ils = RangedHtml5 $
-    img_ ([src_ (escapeURI target), alt_ (renderAlt $ unRangedHtml5 ils)] ++
-                                   [title_ title | not (T.null title)])
-  code t = RangedHtml5 $ code_ (toHtml t)
   rawInline f t
     | f == Format "html" = RangedHtml5 $ span_ $ toHtmlRaw t
     | otherwise          = mempty
+  lineBreak = coerce (lineBreak :: Html5)
+  softBreak = coerce (softBreak :: Html5)
+  emph ils = coerce (emph (coerce ils) :: Html5)
+  strong ils = coerce (strong (coerce ils) :: Html5)
+  link target title ils = coerce (link target title (coerce ils) :: Html5)
+  image target title ils = coerce (image target title (coerce ils) :: Html5)
+  code t = coerce (code t :: Html5)
 
 instance IsBlock RangedHtml5 RangedHtml5 where
-  paragraph ils = RangedHtml5 $ p_ (unRangedHtml5 ils) <> nl
-  plain ils = RangedHtml5 $ unRangedHtml5 ils <> nl
-  thematicBreak = RangedHtml5 $ hr_ [] <> nl
-  blockQuote bs = RangedHtml5 $ blockquote_ (nl <> unRangedHtml5 bs) <> nl
-  codeBlock info t = RangedHtml5 $ pre_ (with code_ attr (toHtml t)) <> nl
-    where attr = [class_ ("language-" <> lang) | not (T.null info)]
-          lang = T.takeWhile (not . isSpace) info
-  header level ils = RangedHtml5 $ h (unRangedHtml5 ils) <> nl
-    where h = case level of
-                   1 -> h1_
-                   2 -> h2_
-                   3 -> h3_
-                   4 -> h4_
-                   5 -> h5_
-                   6 -> h6_
-                   _ -> p_
-  rawBlock f t
-    | f == Format "html" = RangedHtml5 $ toHtmlRaw t
-    | otherwise          = mempty
-  referenceLinkDefinition _ _ = mempty
-  list (BulletList _) lSpacing items = RangedHtml5 $ ul_
-    (nl <> mconcat (map (li . unRangedHtml5) items)) <> nl
-   where li x = if lSpacing == TightList
-                   then li_ x <> nl
-                   else li_ (nl <> x) <> nl
-  list (OrderedList startnum _) lSpacing items = RangedHtml5 $ with ol_ attr
-    (nl <> mconcat (map (li . unRangedHtml5) items)) <> nl
-   where li x = if lSpacing == TightList
-                   then li_ x <> nl
-                    else li_ (nl <> x) <> nl
-         attr = [start_ (T.pack (show startnum)) | startnum /= 1]
+  paragraph ils = coerce (paragraph (coerce ils) :: Html5)
+  plain ils = coerce (plain (coerce ils) :: Html5)
+  thematicBreak = coerce (thematicBreak :: Html5)
+  blockQuote bs = coerce (blockQuote (coerce bs) :: Html5)
+  codeBlock info t = coerce (codeBlock info t :: Html5)
+  header level ils = coerce (header level (coerce ils) :: Html5)
+  rawBlock f t = coerce (rawBlock f t :: Html5)
+  referenceLinkDefinition x y = coerce (referenceLinkDefinition x y :: Html5)
+  list lt lSpacing items = coerce (list lt lSpacing (coerce items) :: Html5)
 
 instance Rangeable RangedHtml5 where
   ranged r (RangedHtml5 x) =
@@ -189,8 +164,8 @@ instance HasMath Html5 where
     span_ [class_ ("math display")] ("\\[" <> toHtml t <> "\\]")
 
 instance HasMath RangedHtml5 where
-  inlineMath t = RangedHtml5 (unHtml5 $ inlineMath t)
-  displayMath t = RangedHtml5 (unHtml5 $ displayMath t)
+  inlineMath t = coerce (inlineMath t :: Html5)
+  displayMath t = coerce (displayMath t :: Html5)
 
 instance HasPipeTable Html5 Html5 where
   pipeTable aligns headerCells rows = Html5 $ do
@@ -219,23 +194,36 @@ instance HasPipeTable Html5 Html5 where
     "\n"
 
 instance HasPipeTable RangedHtml5 RangedHtml5 where
-  pipeTable aligns headerCells rows =
-    RangedHtml5 $ unHtml5 $ pipeTable aligns
-      (map (Html5 . unRangedHtml5) headerCells)
-      (map (map (Html5 . unRangedHtml5)) rows)
+  pipeTable aligns headerCells rows = coerce
+    (pipeTable aligns (map coerce headerCells :: [Html5])
+      (map (map coerce) rows :: [[Html5]]) :: Html5)
 
 instance HasStrikethrough Html5 where
   strikethrough ils = Html5 $ del_ (unHtml5 ils)
 
 instance HasStrikethrough RangedHtml5 where
-  strikethrough (RangedHtml5 ils) = RangedHtml5 $ del_ ils
+  strikethrough ils = coerce (strikethrough (coerce ils) :: Html5)
 
 instance HasFootnote Html5 Html5 where
-  footnote = undefined -- TODO
-  footnoteList = undefined
-  footnoteRef = undefined
+  footnote num lab' x = Html5 $ do
+   with div_ [class_ "footnote", id_ ("fn-" <> lab')] $ do
+     "\n"
+     with div_ [class_ "footnote-number"] $ do
+       "\n"
+       a_ [href_ ("#fnref-" <> lab')] $ toHtml (show num)
+     with div_ [class_ "footnote-contents"] $ do
+       "\n"
+       coerce x
+     "\n"
+   "\n"
+  footnoteList items = Html5 $ do
+    with section_ [class_ "footnotes"] $ "\n" <> coerce (mconcat items)
+    "\n"
+  footnoteRef x lab _ = Html5 $
+    with sup_ [class_ "footnote-ref"] $
+      a_ [href_ ("#fn-" <> lab), id_ ("fnref-" <> lab)] $ toHtml x
 
 instance HasFootnote RangedHtml5 RangedHtml5 where
-  footnote = undefined -- TODO
-  footnoteList = undefined
-  footnoteRef = undefined
+  footnote num lab' x = coerce (footnote num lab' (coerce x) :: Html5)
+  footnoteList items = coerce (footnoteList (coerce items) :: Html5)
+  footnoteRef x lab y = coerce (footnoteRef (coerce x) lab (coerce y) :: Html5)

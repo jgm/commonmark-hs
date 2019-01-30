04a0d51e1e27197f409ec1e42b9ad4731defc45e
John MacFarlane <jgm@berkeley.edu>
2019-01-29 22:26:27 -0800

    Added shell of Attributes extension.
    This will hold specs for headers, images, links,
    code, with attributes (overriding standard specs).

---
+++ commonmark/src/Commonmark/Extensions/Attributes.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Commonmark.Extensions.Attributes
+  ( Attributes(..)
+  , headerAttributesSpec
+  )
+where
+import Commonmark.Types
+import Commonmark.Tokens
+import Commonmark.Syntax
+import Commonmark.Inlines
+import Commonmark.SourceMap
+import Commonmark.Util
+import Commonmark.Html (escapeHtml)
+import Text.Parsec
+import Data.Text (Text)
+import Data.Semigroup (Semigroup(..))
+import Data.Text.Lazy.Builder (Builder)
+
+headerAttributesSpec :: (Monad m, IsBlock il bl, IsInline il, HasAttributes bl)
+                     => SyntaxSpec m il bl
+headerAttributes = SyntaxSpec
+  { syntaxBlockSpecs = []
+  , syntaxBracketedSpecs = []
+  , syntaxFormattingSpecs = []
+  , syntaxInlineParsers = []
+  , syntaxFinalParsers = []
+  }
+
+type Attributes = [(Text, Text)]

1a5fe7aa277cf64d241a8de3d4a68f222f0c057d
John MacFarlane <jgm@berkeley.edu>
2019-02-17 12:27:58 -0800

    Use manyTill instead of whileM_ and getInput.

--- commonmark/src/Commonmark/Blocks.hs
+++ commonmark/src/Commonmark/Blocks.hs
@@ -88,7 +88,7 @@ processLines :: (Monad m, IsBlock il bl)
              -> [BlockParser m il bl bl] -- ^ Parsers to run at end
              -> BlockParser m il bl bl
 processLines specs finalParsers = do
-  whileM_ (not . null <$> getInput) (processLine specs)
+  void $ manyTill (processLine specs) eof
   tree <- (nodeStack <$> getState) >>= collapseNodeStack
   body <- blockConstructor (blockSpec (rootLabel tree)) tree
   endContent <- mconcat <$> sequence finalParsers

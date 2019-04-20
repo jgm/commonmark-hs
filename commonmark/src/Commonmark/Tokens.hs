9dae1528b0c5534a8e2477cdbd4fb4077d333c9e
John MacFarlane <jgm@berkeley.edu>
2019-04-19 22:17:50 -0700

    Fix source position updating, add new versions of char parsers.

--- commonmark/src/Commonmark/Tokens.hs
+++ commonmark/src/Commonmark/Tokens.hs
@@ -10,15 +10,24 @@ module Commonmark.Tokens
   , SourcePos
   , toSubject
   , tok
+  , satisfy
+  , char
+  , anyChar
+  , oneOf
+  , noneOf
   , withRaw
   , charBehind
   , getOffset
+  , gobbleSpaces
+  , gobbleUpToSpaces
   ) where
 
-import           Text.Parsec
+import           Text.Parsec hiding (satisfy, char, anyChar, oneOf, noneOf)
 import           Data.Data       (Data, Typeable)
 import qualified Data.Vector.Unboxed as V
 import           Data.List       (mapAccumL)
+
+import Debug.Trace
 
 type Offset = Int
 
@@ -75,11 +84,8 @@ tok = tokenPrim pprint updatePos
     pprint c = [c]
     updatePos :: SourcePos -> Char -> Subject -> SourcePos
     updatePos spos _ subj =
-      case subjectRanges subj of
-             [] -> spos
-             (cur,_):_ ->
-                let (!ln, !col) = subjectPositions subj V.! cur
-                in  setSourceLine (setSourceColumn spos col) ln
+      let (!ln, !col) = subjectPositions subj V.! subjectOffset subj
+      in  setSourceLine (setSourceColumn spos col) ln
 
 withRaw :: Monad m => ParsecT Subject u m a -> ParsecT Subject u m (a, [Char])
 withRaw p = do
@@ -114,3 +120,57 @@ charBehind = do
       | offset > start
       , offset <= end -> return $ subjectChars subj V.!? (offset - 1)
     _                 -> return Nothing
+
+satisfy :: Monad m => (Char -> Bool) -> ParsecT Subject u m Char
+satisfy f = tok (\c -> if f c then Just c else Nothing)
+
+anyChar :: Monad m => ParsecT Subject u m Char
+anyChar = tok Just
+
+char :: Monad m => Char -> ParsecT Subject u m Char
+char c = satisfy (== c)
+
+oneOf :: Monad m => [Char] -> ParsecT Subject u m Char
+oneOf cs = satisfy (`elem` cs)
+
+noneOf :: Monad m => [Char] -> ParsecT Subject u m Char
+noneOf cs = satisfy (`notElem` cs)
+
+-- | Parses exactly @n@ spaces. If tabs are encountered,
+-- they are split into spaces before being consumed; so
+-- a tab may be partially consumed by this parser.
+gobbleSpaces :: (Monad m) => Int -> ParsecT Subject u m Int
+gobbleSpaces 0 = return 0
+gobbleSpaces n = try $ gobble' True n
+{-# INLINEABLE gobbleSpaces #-}
+
+-- | Parses up to @n@ spaces.
+gobbleUpToSpaces :: (Monad m) => Int -> ParsecT Subject u m Int
+gobbleUpToSpaces 0 = return 0
+gobbleUpToSpaces n = gobble' False n
+{-# INLINEABLE gobbleUpToSpaces #-}
+
+gobble' :: (Monad m) => Bool -> Int -> ParsecT Subject u m Int
+gobble' requireAll numspaces
+  | numspaces >= 1 =
+   try (satisfy (== ' ') >> (+1) <$> gobble' requireAll (numspaces - 1))
+   <|>
+   (do startpos <- getPosition
+       endpos <- lookAhead $ satisfy (== '\t') *> getPosition
+       return $! traceShowId $! (startpos, endpos)
+       case sourceColumn endpos - sourceColumn startpos of
+         n | n < numspaces  -> do
+               char '\t'
+               (+ n) <$> gobble' requireAll (numspaces - n)
+           | n == numspaces -> do
+               char '\t'
+               return n
+           | otherwise      -> do
+               -- leave \t on input stream
+               setPosition $ incSourceColumn startpos numspaces
+               return numspaces)
+    <|> if requireAll
+           then fail "not enough spaces to gobble"
+           else return 0
+  | otherwise     = return 0
+{-# INLINEABLE gobble' #-}

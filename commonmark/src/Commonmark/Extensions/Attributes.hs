db3d9e1ca88cf6e8ac9b503a80f559a47d234fda
John MacFarlane <jgm@berkeley.edu>
2019-04-21 09:43:20 -0700

    Class attributes working.

--- commonmark/src/Commonmark/Extensions/Attributes.hs
+++ commonmark/src/Commonmark/Extensions/Attributes.hs
@@ -17,6 +17,7 @@ import Commonmark.Util
 import Commonmark.Blocks
 import Commonmark.Html (escapeHtml, addAttribute, HtmlAttribute)
 import Data.Dynamic
+import qualified Data.Text as T
 import Data.Tree
 import Control.Monad (mzero)
 import Text.Parsec
@@ -73,11 +74,46 @@ parseAttributes ts =
        ((,) <$> many (notFollowedBy pAttributes >> anyTok)
             <*> option [] pAttributes) "heading contents" ts of
     Left _        -> (ts, [])
-    Right (xs,ys) -> (xs, ys)
+    Right (xs,ys) -> (xs, collapseAttrs ys)
+  where
+    collapseAttrs xs =
+      let classes = [y | ("class", y) <- xs] in
+     (case lookup "id" xs of
+         Just id' -> (("id",id'):)
+         Nothing  -> id) .
+      (if null classes
+          then id
+          else (("class", T.unwords classes):)) $
+      [(k,v) | (k,v) <- xs, k /= "id" && k /= "class"]
 
 pAttributes :: Monad m => ParsecT [Tok] u m Attributes
 pAttributes = try $ do
   symbol '{'
+  optional whitespace
+  let pAttribute = pIdentifier <|> pClass <|> pKeyValue
+  a <- pAttribute
+  as <- many $ try (whitespace *> (pIdentifier <|> pClass <|> pKeyValue))
+  optional whitespace
+  symbol '}'
+  return (a:as)
+
+pIdentifier :: Monad m => ParsecT [Tok] u m HtmlAttribute
+pIdentifier = try $ do
   symbol '#'
-  xs <- manyTill anyTok (symbol '}')
-  return [("id", untokenize xs)]
+  xs <- many1 $
+        satisfyWord (const True)
+    <|> satisfyTok (\c -> hasType (Symbol '-') c || hasType (Symbol '_') c
+                        || hasType (Symbol ':') c || hasType (Symbol '.') c)
+  return ("id", untokenize xs)
+
+pClass :: Monad m => ParsecT [Tok] u m HtmlAttribute
+pClass = do
+  symbol '.'
+  xs <- many1 $
+        satisfyWord (const True)
+    <|> satisfyTok (\c -> hasType (Symbol '-') c || hasType (Symbol '_') c)
+  return ("class", untokenize xs)
+
+pKeyValue :: Monad m => ParsecT [Tok] u m HtmlAttribute
+pKeyValue = mzero
+

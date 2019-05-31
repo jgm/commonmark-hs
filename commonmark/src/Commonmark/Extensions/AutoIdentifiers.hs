40704ae92cdf783bf7bca22ce172da91a1852d07
John MacFarlane <jgm@berkeley.edu>
2019-05-31 14:42:08 -0700

    Generate auto identifiers using GFM's scheme.

--- commonmark/src/Commonmark/Extensions/AutoIdentifiers.hs
+++ commonmark/src/Commonmark/Extensions/AutoIdentifiers.hs
@@ -15,6 +15,9 @@ import Commonmark.Types
 import Commonmark.Tokens
 import Commonmark.Syntax
 import Commonmark.Blocks
+import Data.Char (isAlpha, isLower, isSpace, isUpper, toLower, isAlphaNum,
+                  generalCategory, GeneralCategory(NonSpacingMark,
+                  SpacingCombiningMark, EnclosingMark, ConnectorPunctuation))
 import Data.Dynamic
 import qualified Data.Text as T
 import Data.Tree
@@ -22,7 +25,7 @@ import Data.Traversable
 import Control.Monad (mzero, guard, void)
 import Text.Parsec
 
-autoIdentifiersSpec :: (Monad m, IsBlock il bl, IsInline il)
+autoIdentifiersSpec :: (Monad m, IsBlock il bl, IsInline il, ToPlainText il)
                     => SyntaxSpec m il bl
 autoIdentifiersSpec = mempty
   { syntaxFinalParsers = [addAutoIdentifiers]
@@ -30,7 +33,7 @@ autoIdentifiersSpec = mempty
 
 -- Go through the node stack and add identifiers where they
 -- are missing.
-addAutoIdentifiers :: (Monad m, IsBlock il bl, IsInline il)
+addAutoIdentifiers :: (Monad m, IsBlock il bl, IsInline il, ToPlainText il)
                    => BlockParser m il bl bl
 addAutoIdentifiers = do
   nodes <- nodeStack <$> getState
@@ -38,13 +41,25 @@ addAutoIdentifiers = do
   updateState $ \st -> st{ nodeStack = nodes' }
   return mempty
 
-addId :: (Monad m, IsBlock il bl, IsInline il)
+addId :: (Monad m, IsBlock il bl, IsInline il, ToPlainText il)
        => BlockData m il bl -> BlockParser m il bl (BlockData m il bl)
 addId bd
-  | blockType (blockSpec bd) `elem` ["ATXHeading", "SetextHeading"] =
+  | blockType (blockSpec bd) `elem` ["ATXHeading", "SetextHeading"] = do
     case lookup "id" (blockAttributes bd) of
-      Nothing  -> return $
-                   bd{ blockAttributes =
-                        ("id","dummy") : blockAttributes bd }
+      Nothing  -> do
+        heading <- runInlineParser
+                    (removeIndent . mconcat . reverse . blockLines $ bd)
+        let ident = makeIdentifier (toPlainText heading)
+        return $ bd{ blockAttributes = ("id",ident) : blockAttributes bd }
       Just _   -> return bd
   | otherwise = return bd
+
+makeIdentifier :: T.Text -> T.Text
+makeIdentifier = toIdent
+  where
+    toIdent = filterPunct . spaceToDash . T.map toLower
+    spaceToDash = T.map (\c -> if isSpace c then '-' else c)
+    filterPunct = T.filter (\c -> isSpace c || isAlphaNum c || isAllowedPunct c)
+    isAllowedPunct c = c == '-' || c == '_' ||
+          generalCategory c `elem` [NonSpacingMark, SpacingCombiningMark,
+                                    EnclosingMark, ConnectorPunctuation]

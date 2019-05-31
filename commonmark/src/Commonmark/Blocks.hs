e44fe7bafed098dcd993ae5a0613d13e1fb77161
John MacFarlane <jgm@berkeley.edu>
2019-05-31 08:38:10 -0700

    Run final parsers before body constructor.

--- commonmark/src/Commonmark/Blocks.hs
+++ commonmark/src/Commonmark/Blocks.hs
@@ -101,8 +101,8 @@ processLines :: (Monad m, IsBlock il bl)
 processLines specs finalParsers = do
   skipManyTill (processLine specs) eof
   tree <- (nodeStack <$> getState) >>= collapseNodeStack
-  body <- blockConstructor (blockSpec (rootLabel tree)) tree
   endContent <- mconcat <$> sequence finalParsers
+  body <- blockConstructor (blockSpec (rootLabel tree)) tree
   return $ body <> endContent
 
 processLine :: (Monad m, IsBlock il bl)

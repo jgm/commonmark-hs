c392c18069b82e3532cb273c3f611645c17cc05e
John MacFarlane <jgm@berkeley.edu>
2019-12-01 00:43:39 -0800

    Optimize tokenize with additional strictness in go.
    This reduces allocations quite a bit.

--- commonmark/src/Commonmark/Tokens.hs
+++ commonmark/src/Commonmark/Tokens.hs
@@ -34,46 +34,34 @@ data TokType =
 tokenize :: String -> Text -> [Tok]
 tokenize name = go (initialPos name)
   where
-    go !pos !t =
-      case T.uncons t of
+    go !pos !txt =
+      case T.uncons txt of
         Nothing  -> []
-        Just x   ->
-          let (!tok, !newpos, !t') = getTok pos x
-          in  (tok : go newpos t')
-    getTok !pos (!c, !t) =
-      case c of
-        ' ' -> let (sps,rest) = T.span (==' ') t
-               in  (Tok Spaces pos (T.cons ' ' sps),
-                    incSourceColumn pos (1 + T.length sps),
-                    rest)
-        '\t' -> (Tok Spaces pos "\t",
-                   incSourceColumn pos (4 - (sourceColumn pos - 1) `mod` 4),
-                   t)
-        '\r' -> case T.uncons t of
-                  Just ('\n',t')
-                    -> (Tok LineEnd pos "\r\n",
-                        incSourceLine (setSourceColumn pos 1) 1,
-                        t')
-                  _ -> (Tok LineEnd pos "\r",
-                        incSourceLine (setSourceColumn pos 1) 1,
-                        t)
-        '\n' -> (Tok LineEnd pos "\n",
-                 incSourceLine (setSourceColumn pos 1) 1,
-                 t)
-        _
-         | isAlphaNum c ->
-             let (ws,rest) = T.span isAlphaNum t
-             in  (Tok WordChars pos (T.cons c ws),
-                  incSourceColumn pos (1 + T.length ws),
-                  rest)
-         | isSpace c    ->
-            (Tok UnicodeSpace pos (T.singleton c),
-             incSourceColumn pos 1,
-             t)
-         | otherwise    ->
-            (Tok (Symbol c) pos (T.singleton c),
-             incSourceColumn pos 1,
-             t)
+        Just (!c, !t) ->
+            -- in  tok : go newpos t'
+          case c of
+            ' ' -> let (sps,rest) = T.span (==' ') t
+                   in  Tok Spaces pos (T.cons ' ' sps) :
+                        go (incSourceColumn pos (1 + T.length sps)) rest
+            '\t' -> Tok Spaces pos "\t" :
+                    go (incSourceColumn pos (4 - (sourceColumn pos - 1) `mod` 4)) t
+            '\r' -> case T.uncons t of
+                      Just ('\n',t')
+                        -> Tok LineEnd pos "\r\n" :
+                           go (incSourceLine (setSourceColumn pos 1) 1) t'
+                      _ -> Tok LineEnd pos "\r" :
+                           go (incSourceLine (setSourceColumn pos 1) 1) t
+            '\n' -> Tok LineEnd pos "\n" :
+                    go (incSourceLine (setSourceColumn pos 1) 1) t
+            _ | isAlphaNum c ->
+                  let (ws,rest) = T.span isAlphaNum t
+                  in  Tok WordChars pos (T.cons c ws) :
+                      go (incSourceColumn pos (1 + T.length ws)) rest
+              | isSpace c    ->
+                  Tok UnicodeSpace pos (T.singleton c) : go (incSourceColumn pos 1) t
+              | otherwise    ->
+                  Tok (Symbol c) pos (T.singleton c) :
+                  go (incSourceColumn pos 1) t
 
 -- | Reverses 'tokenize'.  @untokenize . tokenize ""@ should be
 -- the identity.

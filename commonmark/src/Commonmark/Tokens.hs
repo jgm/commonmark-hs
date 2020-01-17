8559521934e97bf23262f9a2cba22afcf1e8fe2b
John MacFarlane <jgm@berkeley.edu>
2020-01-17 09:57:44 -0800

    Tokens: avoid uncons which is slow.
    This gives a noticeable performance boost.

--- commonmark/src/Commonmark/Tokens.hs
+++ commonmark/src/Commonmark/Tokens.hs
@@ -34,36 +34,33 @@ data TokType =
 -- | Convert a 'Text' into a list of 'Tok'. The first parameter
 -- species the source name.
 tokenize :: String -> Text -> [Tok]
-tokenize name = go (initialPos name)
+tokenize name = go (initialPos name) . T.unpack
   where
-    go pos txt =
-      case T.uncons txt of
-        Nothing  -> []
-        Just (c, t) ->
-            -- in  tok : go newpos t'
+    go _pos [] = []
+    go pos  (c:cs) =
           case c of
-            ' ' -> let (sps,rest) = T.span (==' ') t
-                   in  Tok Spaces pos (T.cons ' ' sps) :
-                        go (incSourceColumn pos (1 + T.length sps)) rest
+            ' ' -> let (sps,rest) = span (==' ') cs
+                   in  Tok Spaces pos (T.pack (' ':sps)) :
+                        go (incSourceColumn pos (1 + length sps)) rest
             '\t' -> Tok Spaces pos "\t" :
-                    go (incSourceColumn pos (4 - (sourceColumn pos - 1) `mod` 4)) t
-            '\r' -> case T.uncons t of
-                      Just ('\n',t')
+                    go (incSourceColumn pos (4 - (sourceColumn pos - 1) `mod` 4)) cs
+            '\r' -> case cs of
+                      ('\n':cs')
                         -> Tok LineEnd pos "\r\n" :
-                           go (incSourceLine (setSourceColumn pos 1) 1) t'
+                           go (incSourceLine (setSourceColumn pos 1) 1) cs'
                       _ -> Tok LineEnd pos "\r" :
-                           go (incSourceLine (setSourceColumn pos 1) 1) t
+                           go (incSourceLine (setSourceColumn pos 1) 1) cs
             '\n' -> Tok LineEnd pos "\n" :
-                    go (incSourceLine (setSourceColumn pos 1) 1) t
+                    go (incSourceLine (setSourceColumn pos 1) 1) cs
             _ | isAlphaNum c ->
-                  let (ws,rest) = T.span isAlphaNum t
-                  in  Tok WordChars pos (T.cons c ws) :
-                      go (incSourceColumn pos (1 + T.length ws)) rest
+                  let (ws,rest) = span isAlphaNum cs
+                  in  Tok WordChars pos (T.pack (c : ws)) :
+                      go (incSourceColumn pos (1 + length ws)) rest
               | isSpace c    ->
-                  Tok UnicodeSpace pos (T.singleton c) : go (incSourceColumn pos 1) t
+                  Tok UnicodeSpace pos (T.singleton c) : go (incSourceColumn pos 1) cs
               | otherwise    ->
                   Tok (Symbol c) pos (T.singleton c) :
-                  go (incSourceColumn pos 1) t
+                  go (incSourceColumn pos 1) cs
 
 -- | Reverses 'tokenize'.  @untokenize . tokenize ""@ should be
 -- the identity.

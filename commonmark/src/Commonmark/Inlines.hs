384ef1aad9d2b70740958d4e2fd15faf1a59a2da
John MacFarlane <jgm@berkeley.edu>
2020-01-22 16:07:17 -0800

    Fix extra import.

--- commonmark/src/Commonmark/Inlines.hs
+++ commonmark/src/Commonmark/Inlines.hs
@@ -46,7 +46,6 @@ import           Control.Monad              (guard, mzero)
 import           Data.List                  (foldl')
 import           Data.Char                  (isAscii, isLetter)
 import           Data.Dynamic               (Dynamic)
-import           Data.List                  (foldl')
 import qualified Data.IntMap.Strict         as IntMap
 import qualified Data.Map                   as M
 import           Data.Maybe                 (isJust, mapMaybe)

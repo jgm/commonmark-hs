19058783fc69a315c12c75b952848f5fa41cf353
John MacFarlane <jgm@berkeley.edu>
2020-02-01 21:19:24 -0800

    Temporarily comment out extensions.

--- commonmark/src/Commonmark.hs
+++ commonmark/src/Commonmark.hs
@@ -4,20 +4,20 @@ module Commonmark
     , module Commonmark.Syntax
     , module Commonmark.Parser
     , module Commonmark.SourceMap
-    , module Commonmark.Extensions.Smart
-    , module Commonmark.Extensions.Strikethrough
-    , module Commonmark.Extensions.Superscript
-    , module Commonmark.Extensions.Subscript
-    , module Commonmark.Extensions.PipeTable
-    , module Commonmark.Extensions.Math
-    , module Commonmark.Extensions.Emoji
-    , module Commonmark.Extensions.Autolink
-    , module Commonmark.Extensions.Footnote
-    , module Commonmark.Extensions.DefinitionList
-    , module Commonmark.Extensions.Attributes
-    , module Commonmark.Extensions.AutoIdentifiers
-    , module Commonmark.Extensions.FancyList
-    , module Commonmark.Extensions.ImplicitHeadingReferences
+    -- , module Commonmark.Extensions.Smart
+    -- , module Commonmark.Extensions.Strikethrough
+    -- , module Commonmark.Extensions.Superscript
+    -- , module Commonmark.Extensions.Subscript
+    -- , module Commonmark.Extensions.PipeTable
+    -- , module Commonmark.Extensions.Math
+    -- , module Commonmark.Extensions.Emoji
+    -- , module Commonmark.Extensions.Autolink
+    -- , module Commonmark.Extensions.Footnote
+    -- , module Commonmark.Extensions.DefinitionList
+    -- , module Commonmark.Extensions.Attributes
+    -- , module Commonmark.Extensions.AutoIdentifiers
+    -- , module Commonmark.Extensions.FancyList
+    -- , module Commonmark.Extensions.ImplicitHeadingReferences
     ) where
 
 import           Commonmark.Tokens
@@ -25,18 +25,18 @@ import           Commonmark.Types
 import           Commonmark.Parser
 import           Commonmark.Syntax
 import           Commonmark.SourceMap
-import           Commonmark.Extensions.Smart
-import           Commonmark.Extensions.Strikethrough
-import           Commonmark.Extensions.Superscript
-import           Commonmark.Extensions.Subscript
-import           Commonmark.Extensions.PipeTable
-import           Commonmark.Extensions.Math
-import           Commonmark.Extensions.Emoji
-import           Commonmark.Extensions.Autolink
-import           Commonmark.Extensions.Footnote
-import           Commonmark.Extensions.DefinitionList
-import           Commonmark.Extensions.Attributes
-import           Commonmark.Extensions.AutoIdentifiers
-import           Commonmark.Extensions.FancyList
-import           Commonmark.Extensions.ImplicitHeadingReferences
+-- import           Commonmark.Extensions.Smart
+-- import           Commonmark.Extensions.Strikethrough
+-- import           Commonmark.Extensions.Superscript
+-- import           Commonmark.Extensions.Subscript
+-- import           Commonmark.Extensions.PipeTable
+-- import           Commonmark.Extensions.Math
+-- import           Commonmark.Extensions.Emoji
+-- import           Commonmark.Extensions.Autolink
+-- import           Commonmark.Extensions.Footnote
+-- import           Commonmark.Extensions.DefinitionList
+-- import           Commonmark.Extensions.Attributes
+-- import           Commonmark.Extensions.AutoIdentifiers
+-- import           Commonmark.Extensions.FancyList
+-- import           Commonmark.Extensions.ImplicitHeadingReferences
 

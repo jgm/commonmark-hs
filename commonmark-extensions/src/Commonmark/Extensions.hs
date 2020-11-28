2c2353eaa8641848b5172e9eb32e39cf4bcc9492
John MacFarlane <jgm@berkeley.edu>
2020-11-28 10:42:56 -0800

    Add shell for Commonmark.Extensions.Citations.

--- commonmark-extensions/src/Commonmark/Extensions.hs
+++ commonmark-extensions/src/Commonmark/Extensions.hs
@@ -39,6 +39,7 @@ module Commonmark.Extensions
     , module Commonmark.Extensions.FancyList
     , module Commonmark.Extensions.TaskList
     , module Commonmark.Extensions.ImplicitHeadingReferences
+    , module Commonmark.Extensions.Citations
     , gfmExtensions
     ) where
 
@@ -58,6 +59,7 @@ import           Commonmark.Extensions.AutoIdentifiers
 import           Commonmark.Extensions.FancyList
 import           Commonmark.Extensions.TaskList
 import           Commonmark.Extensions.ImplicitHeadingReferences
+import           Commonmark.Extensions.Citations
 import           Commonmark
 #if !MIN_VERSION_base(4,11,0)
 import           Data.Monoid ((<>))
@@ -71,4 +73,3 @@ gfmExtensions :: (Monad m, IsBlock il bl, IsInline il,
 gfmExtensions =
   emojiSpec <> strikethroughSpec <> pipeTableSpec <> autolinkSpec <>
     autoIdentifiersSpec <> taskListSpec
-

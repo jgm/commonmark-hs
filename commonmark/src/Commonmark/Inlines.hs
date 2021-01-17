c090a4ac7e2715b5da2a2a6fa1974cae4cd55ec6
John MacFarlane <jgm@berkeley.edu>
2021-01-17 14:35:02 -0800

    Fix haddocks.

--- commonmark/src/Commonmark/Inlines.hs
+++ commonmark/src/Commonmark/Inlines.hs
@@ -256,13 +256,12 @@ data BracketedSpec il = BracketedSpec
      , bracketedSuffixEnd :: Maybe Char -- ^ Suffix character.
      , bracketedSuffix    :: ReferenceMap
                           -> [([Tok], il)]
-                          -- ^ Contents of bracketed part: each chunk
-                          -- consists of a list of tokens and the corresponding
-                          -- parsed inline.
                           -> Parsec [Tok] () (il -> il)
                           -- ^ Parser for suffix after
                           -- brackets.  Returns a constructor.
-                          -- Second parameter is the raw key.
+                          -- Second parameter is contents of bracketed part:
+                          -- each chunk consists of a list of tokens and
+                          -- the corresponding parsed inline.
      }
 
 instance Show (BracketedSpec il) where
